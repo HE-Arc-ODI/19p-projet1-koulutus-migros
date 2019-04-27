a4628c566410118cb337fb5561546a03fc50dbf8
kseewer <40600885+kseewer@users.noreply.github.com>
2019-04-27 13:44:43 +0200

    ajout de getall getby id et delete de Courses et participant
    à modifier selon les classe business et persistanceService

--- src/main/java/ch/hearc/odi/koulutus/rest/CourseResource.java
+++ src/main/java/ch/hearc/odi/koulutus/rest/CourseResource.java
@@ -1,4 +1,58 @@
 package ch.hearc.odi.koulutus.rest;
+import ch.hearc.odi.koulutus.services.PersistenceService;
+
+import java.text.ParseException;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("resource")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 
 public class CourseResource {
+
+    @Inject
+    private PersistenceService persistenceService;
+
+
+    @GET
+    public List<Resource> ResourceGet() {
+        return persistenceService.getAllResources();
+    }
+
+    @GET
+    @Path("{id}")
+    public Resource getResourceId(@PathParam("id") Long customerId) {
+        try {
+            return persistenceService.getResourceById(resourceId);
+        } catch (ResourceException e) {
+            e.printStackTrace();
+            throw new NotFoundException("the Resource does not exist");
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteResource(@PathParam("id") Long id) throws ResourceException {
+        try {
+            persistenceService.deleteResource(id);
+        } catch (ResourceException e) {
+            e.printStackTrace();
+            throw new ResourceException("Resource could not be deleted");
+        }
+    }
 }
+
+
--- src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
+++ src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
@@ -1,4 +1,59 @@
 package ch.hearc.odi.koulutus.rest;
 
+import ch.hearc.odi.koulutus.services.PersistenceService;
+
+import java.text.ParseException;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+
+@Path("customer")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+
 public class ParticipantResource {
+
+
+    @Inject
+    private PersistenceService persistenceService;
+
+    @GET
+    public List<Participant> participantGet() {
+        return persistenceService.getAllParticipant();
+    }
+
+
+    @GET
+    @Path("{id}")
+    public Participant getParticipantId(@PathParam("id") Long customerId) {
+        try {
+            return persistenceService.getParticipantById(resourceId);
+        } catch (ParticipantException e) {
+            e.printStackTrace();
+            throw new NotFoundException("the Participant does not exist");
+        }
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void deleteParticipant(@PathParam("id") Long id) throws ResourceException {
+        try {
+            persistenceService.deleteParticipant(id);
+        } catch (ParticipantException e) {
+            e.printStackTrace();
+            throw new ParticipantException("Participant could not be deleted");
+        }
+    }
 }
